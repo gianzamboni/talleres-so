b1484dd6b63662f0cefb17e919d25bea320c4c5c
Zamboni Gianfranco <gzamboni@ws22.labo4>
2016-03-31 19:48:57 -0300

    Taller ptrace

---
+++ taller-ptrace/hai2.c
@@ -0,0 +1,83 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+using namespace std;
+
+int counter = 0;
+
+pid_t child;
+int status;
+
+
+void increaseCounter(int signum){
+	counter++;
+	fprintf(stdout, "ya va!\n");
+	
+}
+
+void waitChild(int signum){
+		while(1) {
+			if (wait4(-1, &status,0,0) < 0) { perror("waitpid"); break; }
+			if (WIFEXITED(status) && WEXITSTATUS(status) == 0) break; /* Proceso terminado */
+		}
+}
+
+
+int main(int argc, char* argv[]) {
+
+	
+	if (argc <= 1) {
+		fprintf(stderr, "Uso: %s commando [argumentos ...]\n", argv[0]);
+		exit(1);
+	}
+
+	/* Fork en dos procesos */
+	child = fork();
+	if (child == -1) { perror("ERROR fork"); return 1; }
+	if (child == 0) { //Comportamiento del hijo
+		
+		signal(SIGURG, increaseCounter);
+
+		while(counter < 5){
+
+		}
+
+		kill(getppid(), SIGINT);
+		execvp(argv[1], argv+1);
+		/* Si vuelve de exec() hubo un error */
+
+	} else { // Comportamiento del padre
+		/* S'olo se ejecuta en el Padre */
+
+		signal(SIGINT, waitChild);
+		sigset_t emptySet;
+		sigemptyset(&emptySet);
+
+		sigset_t toBlock;
+		sigemptyset(&toBlock);
+		sigaddset(&toBlock, SIGCHLD);
+
+		timespec tiempo;
+		tiempo.tv_sec = 1;
+		tiempo.tv_nsec = 0;
+
+		timespec resto;
+		while(counter < 5){
+
+			sigprocmask(SIG_BLOCK, &toBlock, 0);
+			signal(SIGCHLD, 0);
+			sigprocmask(SIG_BLOCK, &emptySet, 0);
+			nanosleep(&tiempo, &resto);
+			fprintf(stderr, "sup!\n");
+			kill(child, SIGURG);
+			counter ++;
+		}
+	}
+	
+	return 0;
+}
---
+++ taller-ptrace/launch.c
@@ -0,0 +1,35 @@
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/reg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main(int argc, char* argv[]) {
+	int status;
+	pid_t child;
+
+	if (argc <= 1) {
+		fprintf(stderr, "Uso: %s commando [argumentos ...]\n", argv[0]);
+		exit(1);
+	}
+
+	/* Fork en dos procesos */
+	child = fork();
+	if (child == -1) { perror("ERROR fork"); return 1; }
+	if (child == 0) {
+		/* S'olo se ejecuta en el Hijo */
+		execvp(argv[1], argv+1);
+		/* Si vuelve de exec() hubo un error */
+		perror("ERROR child exec(...)"); exit(1);
+	} else {
+		/* S'olo se ejecuta en el Padre */
+		while(1) {
+			if (wait(&status) < 0) { perror("waitpid"); break; }
+			if (WIFEXITED(status)) break; /* Proceso terminado */
+		}
+	}
+	return 0;
+}
