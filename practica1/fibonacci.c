ba1d6e1c9ad892347a4cf510e88cab74da2ccddb
Zamboni Gianfranco <gzamboni@ws12.labo4>
2016-04-05 17:29:17 -0300

    Practica 1

--- practica1/fibonacci.c
+++ practica1/fibonacci.c
@@ -2,7 +2,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/reg.h>
-#include <sys/syscall.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,28 +21,15 @@ int main(int argc, char* argv[]) {
 	if (child == -1) { perror("ERROR fork"); return 1; }
 	if (child == 0) {
 		/* S'olo se ejecuta en el Hijo */
-		int trace = ptrace(PTRACE_TRACEME, 0, 0, 0);
 		execvp(argv[1], argv+1);
-	
+		/* Si vuelve de exec() hubo un error */
+		perror("ERROR child exec(...)"); exit(1);
 	} else {
 		/* S'olo se ejecuta en el Padre */
 		while(1) {
 			if (wait(&status) < 0) { perror("waitpid"); break; }
 			if (WIFEXITED(status)) break; /* Proceso terminado */
-			
-			int sys_no = ptrace(PTRACE_PEEKUSER, child, 8*ORIG_RAX, 0);
-			if(sys_no == SYS_kill) {
-				printf("Se ha hecho justicia\n");
-				ptrace(PTRACE_KILL, child,0,0);
-				break;
-			} else{
-				int syscall_enter = ptrace(PTRACE_SYSCALL, child, 0, 0);
-			}	
-			
-
 		}
-
-		ptrace(PTRACE_DETACH, child, 0,0);
 	}
 	return 0;
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
-## Practica 1
-
-El ejericio de los simpsons (ejercicio 6) no esta terminado, falta arreglar que homero espere a los tres hijos
+## Taller Ptrace Ejercicio 1
+1. Ejercicio 1 completo
+   
+   El ejericio de los simpsons (ejercicio 6) no esta terminado, falta arreglar que homero espere a los tres hijos
