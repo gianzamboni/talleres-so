1425e766911d1add89b5a4271fe69c650cac757c
Gian150 <gianfranco376@gmail.com>
2016-06-19 18:53:40 -0300

    Un palo

--- taller-ipc/ejercicios/mini-telnet-2/mt_client2.c
+++ taller-ipc/ejercicios/mini-telnet-2/mt_client2.c
@@ -1,7 +1,56 @@
 #include "mt.h"
 
 int main(int argc, char* argv[]) {
-    /* TODO: Implementar */
+    int                 socket_fd, read_size;
+    struct sockaddr_in  remote;
+    char                buf[MAX_MSG_LENGTH];
+
+    if(argc != 2) {
+        printf("\n Usage: %s 127.0.0.1\n",argv[0]);
+        exit(1);
+    } 
+
+    char* the_ip = argv[1];
+
+    /* Crear un socket de tipo UNIX con SOCK_STREAM */
+    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("creando socket");
+        exit(1);
+    }
+
+
+    /* Establecer la dirección a la cual conectarse. */
+    remote.sin_family = AF_INET;
+    remote.sin_port = htons(PORT);
+
+    if(inet_aton(the_ip, &remote.sin_addr) < 0){
+    	perror("inet aton error");
+    	exit(1);
+    }
+
+    /* Conectarse. */
+    if (connect(socket_fd, (struct sockaddr *)&remote, sizeof(remote)) == -1) {
+        perror("conectandose");
+        exit(1);
+    }
+
+    /* Establecer la dirección a la cual conectarse para escuchar. */
+    while(printf("> "), fgets(buf, MAX_MSG_LENGTH, stdin), !feof(stdin)) {
+        if (send(socket_fd, buf, strlen(buf), 0) == -1) {
+            perror("enviando");
+            exit(1);
+        }
+
+        if(!strncmp(buf, END_STRING, MAX_MSG_LENGTH)) {
+        	break;
+        } else {
+        	read_size =  read(socket_fd, buf, MAX_MSG_LENGTH);
+        	printf("Recibido: %.*s\n", read_size, buf);
+        }
+    }
+
+    /* Cerrar el socket. */
+    close(socket_fd);
 
     return 0;
 }
--- taller-ipc/ejercicios/mini-telnet-2/mt_server2.c
+++ taller-ipc/ejercicios/mini-telnet-2/mt_server2.c
@@ -1,8 +1,59 @@
 #include "mt.h"
 
 int main(int argc, char* argv[]) {
-    /* TODO: Implementar */
+    int                 sock, remote_sock, remote_sock_size;
+    struct sockaddr_in  local, remote;
+    char                buf[MAX_MSG_LENGTH];
+
+    /* Crear socket sobre el que se lee: dominio INET, protocolo UDP (DGRAM). */
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("abriendo socket");
+        exit(1);
+    }
+
+    /* Crear nombre, usamos INADDR_ANY para indicar que cualquiera puede enviar aquí. */
+    local.sin_family = AF_INET;
+    local.sin_addr.s_addr = INADDR_ANY;
+    local.sin_port = htons(PORT);
+
+    if (bind(sock, (void*) &local, sizeof(local))) {
+        perror("binding stream socket");
+        exit(1);
+    }
+
+	/* Escuchar en el socket y permitir 5 conexiones en espera. */
+    if(listen(sock, 5) == -1){
+    	perror("escuchando");
+    	exit(1);
+    }
+
+
+	/* Aceptar una conexión entrante. */
+    remote_sock_size = sizeof(remote);
+    if ((remote_sock = accept(sock, (struct sockaddr*) &remote, (socklen_t*) &remote_sock_size)) == -1) {
+        perror("aceptando la conexión entrante");
+        exit(1);
+    }
+
+    dup2(remote_sock, 1); //Redirigiendo stdout
+    dup2(remote_sock, 2); //Redirigiendo stderr
+
+    /* Recibimos mensajes hasta que alguno sea el que marca el final. */
+    for (;;) {
+        memset(buf, 0, MAX_MSG_LENGTH);
+        read(remote_sock, buf, MAX_MSG_LENGTH);
+        if (strncmp(buf, END_STRING, MAX_MSG_LENGTH) == 0)
+            break;
+        printf("Comando: %s", buf);
+        system(buf);
+    }
+
+    /* Cerrar socket de recepción. */
+    close(remote_sock);
+    close(sock);
 
     return 0;
 }
+
 
